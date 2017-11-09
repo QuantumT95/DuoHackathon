08e104596584441491ba957a907e2626047b3dcc
Ed Solis <edsfocci@gmail.com>
2017-11-09 01:14:19 -0600

    Ajax call to gs-rest-service from Registration

--- public/jquery/regButton.js
+++ public/jquery/regButton.js
@@ -1,3 +1,16 @@
-$("#btn").click(function(){
-	alert("hi");
-})
+$(function() {
+	$("#btn").click(function(e){
+		e.preventDefault();
+
+		$.get({
+			url: 'http://localhost:8080/enroll',
+			data: {
+				name: $('#username').val()
+			},
+			success: function(response) {
+				console.log(response);
+				$('.panel-body').html(JSON.stringify(response));
+			}
+		});
+	});
+});
