cd14d64e0fc276546b269a7c3ed8d8d4b23ac5d3
QuantumT95 <tran.charlie@ymail.com>
2017-11-09 05:49:56 -0600

    fixed navbar

--- public/jquery/regButton.js
+++ public/jquery/regButton.js
@@ -1,4 +1,11 @@
-$("#regForm").submit(function(event){
-	event.preventDefault();
-	return false;
+$(function(){
+	$("#submit").on("click", function(event){
+		event.preventDefault();
+		userData={
+			email: $("#email").val(),
+			username: $("#username").val(),
+			password: $("#pwd").val()
+		}
+		console.log(userData);
+	})
 })
