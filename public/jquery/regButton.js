46786864da9096fd9af8c6a4d6689d655a6e5c12
Danny Nhoisaykham <danny@MBP.local>
2017-11-09 10:27:14 -0600

    fix registration button

--- public/jquery/regButton.js
+++ public/jquery/regButton.js
@@ -1,11 +1,9 @@
-$(function(){
-	$("#submit").on("click", function(event){
-		event.preventDefault();
-		userData={
-			email: $("#email").val(),
-			username: $("#username").val(),
-			password: $("#pwd").val()
-		}
-		console.log(userData);
-	})
-})
+$(".submit-btn").on("click", function(event) {
+  event.preventDefault();
+  userData = {
+    email: $("#email").val(),
+    username: $("#username").val(),
+    password: $("#pwd").val()
+  };
+  console.log(userData);
+});
